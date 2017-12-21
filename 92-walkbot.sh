859405b1db325200de87a0853283e0e0637d53a4
BenCat07 <BenCat07@gmail.com>
2017-12-21 14:59:53 +0100

    Add files via upload

--- 92-walkbot.sh
+++ 92-walkbot.sh
@@ -1,4 +1,8 @@
+echo "WARNING: Your current walkbot Paths Will be deleted"
+read -p "Press enter to continue or Ctrl+C (close) to stop."
+sudo rm -rf /opt/cathook/data/walkbot
 sudo mkdir -p /opt/cathook/data/walkbot
 sudo cp walkbot /opt/cathook/data
+sudo rm /opt/steamapps/common/Team\ Fortress\ 2/tf/casual_criteria.vdf
 sudo cp casual_criteria.vdf /opt/steamapps/common/Team\ Fortress\ 2/tf
 chmod 777 /opt/steamapps/common/Team\ Fortress\ 2/tf/casual_criteria.vdf
