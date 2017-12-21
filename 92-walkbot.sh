e3f529964f41f0881e19557648cb4ec2e36f2456
BenCat07 <BenCat07@gmail.com>
2017-12-21 15:01:14 +0100

    Add files via upload

--- 92-walkbot.sh
+++ 92-walkbot.sh
@@ -3,6 +3,7 @@ read -p "Press enter to continue or Ctrl+C (close) to stop."
 sudo rm -rf /opt/cathook/data/walkbot
 sudo mkdir -p /opt/cathook/data/walkbot
 sudo cp walkbot /opt/cathook/data
-sudo rm /opt/steamapps/common/Team\ Fortress\ 2/tf/casual_criteria.vdf
+sudo chmod 777 /opt/cathook/data/walkbot
+sudo rm /opt/steamapps/common/Team\ Fortress\ 2/tf/casual_criteria.vdf	
 sudo cp casual_criteria.vdf /opt/steamapps/common/Team\ Fortress\ 2/tf
-chmod 777 /opt/steamapps/common/Team\ Fortress\ 2/tf/casual_criteria.vdf
+sudo chmod 777 /opt/steamapps/common/Team\ Fortress\ 2/tf/casual_criteria.vdf
