99c506ed4129027120f47d962d088398f17fd1b4
BenCat07 <BenCat07@gmail.com>
2017-12-21 14:54:46 +0100

    Updated Catbot-setup for more walkbot files
    Updated Catbot-setup to have all ctf and koth Walkmaps and additionally Hightower, Run ./92 to Install all.

---
+++ 92-walkbot.sh
@@ -0,0 +1,4 @@
+sudo mkdir -p /opt/cathook/data/walkbot
+sudo cp walkbot /opt/cathook/data
+sudo cp casual_criteria.vdf /opt/steamapps/common/Team\ Fortress\ 2/tf
+chmod 777 /opt/steamapps/common/Team\ Fortress\ 2/tf/casual_criteria.vdf
